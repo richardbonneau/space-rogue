cb0b89ec6e9c7c0613b58f9d202de5200f4f5df0
richardbonneau <richardbonneau91@gmail.com>
2020-10-29 11:24:53 -0400

    still working on the deck

--- Deck/Card.gd
+++ Deck/Card.gd
@@ -1,10 +1,13 @@
-extends ColorRect
+extends Panel
 
 var drag_position = null
 var position_before_drag = null
 
+
+
 func _ready():
 	position_before_drag = self.rect_position
+	
 
 
 
@@ -20,11 +23,24 @@ func _on_Card_gui_input(event):
 	
 	if event is InputEventMouseMotion and drag_position:
 		rect_global_position = get_global_mouse_position() - drag_position
+		if rect_global_position.y < 900:
+			var node_new_style = self.get_stylebox("panel").duplicate()
+			node_new_style.set_border_color(Color("#29be05"))
+			self.add_stylebox_override("panel", node_new_style)
+		else:
+			var node_new_style = self.get_stylebox("panel").duplicate()
+			node_new_style.set_border_color(Color("000"))
+			self.add_stylebox_override("panel", node_new_style)
+			
 
 
 func _on_Card_mouse_entered():
-	self.color = "fff"
+	var node_new_style = self.get_stylebox("panel").duplicate()
+	node_new_style.set_border_color(Color("fff"))
+	self.add_stylebox_override("panel", node_new_style)
 
 
 func _on_Card_mouse_exited():
-	self.color = "000"
+	var node_new_style = self.get_stylebox("panel").duplicate()
+	node_new_style.set_border_color(Color("000"))
+	self.add_stylebox_override("panel", node_new_style)
--- Scenes/Deck.gd
+++ Scenes/Deck.gd
@@ -3,10 +3,4 @@ extends HBoxContainer
 
 
 func _ready():
-	fanCards()
-
-func fanCards():
-	for card in self.get_children():
-		print(card.get_size())
-		card.set_size(Vector2(100,100))
-		print(card.get_size())
+	pass
