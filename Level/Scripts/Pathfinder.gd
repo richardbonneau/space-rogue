ee2d51a36ccb569f22f41d4c3a32ef6a79c88d19
richardbonneau <richardbonneau91@gmail.com>
2020-11-01 15:26:26 -0500

    tiles were too large

--- Level/Scripts/Pathfinder.gd
+++ Level/Scripts/Pathfinder.gd
@@ -1,20 +1,21 @@
 extends Node
 
-
-
 func _ready():
 	pass 
 
-func get_movement_cost_to_destination(var tileA, var tileB):
+func get_movement_cost_to_destination(var tile_a, var tile_b):
 	#Get H value
-	var dstX = abs(tileA.x - tileB.x)
-	var dstZ = abs(tileA.z - tileB.z)
-	print("dstX ",dstX," dstZ ",dstZ, " tileA.x ",tileA," tileB ",tileB)
+	var dst_x = abs(tile_a.x - tile_b.x)
+	var dst_z = abs(tile_a.z - tile_b.z)
+	print(" tile_a.x ",tile_a," tile_b ",tile_b)
 	
-	if dstX > dstZ:
-		return 14 * dstZ + 10 * (dstX - dstZ)
+	if dst_x > dst_z:
+		return 14 * dst_z + 10 * (dst_x - dst_z)
 	else:
-		return 14 * dstX + 10 * (dstZ - dstX)
+		return 14 * dst_x + 10 * (dst_z - dst_x)
+
+func FindPath(var start):
+	pass
 	
 
 
