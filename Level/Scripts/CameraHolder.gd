66ebc958666aa902e250b37423c0fe02bab975fc
richardbonneau <richardbonneau91@gmail.com>
2020-11-05 15:48:32 -0500

    full camera movements

--- Level/Scripts/CameraHolder.gd
+++ Level/Scripts/CameraHolder.gd
@@ -1,40 +1,34 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 func _process(delta):
 	camera_movement(delta)
-	
 
 func camera_movement(var delta):
-	#Move Camera
 	var screen_edge_threshold = 10
 	var screen_scroll_speed = 5
 	var mouse_pos = get_viewport().get_mouse_position()
 	var screen_size = get_viewport().size
 	var move_vec = Vector3()
 	
-	if mouse_pos.x < screen_edge_threshold or Input.is_action_pressed("left"):
-		move_vec.z -= 1
+	#Movements
 	if mouse_pos.y < screen_edge_threshold or Input.is_action_pressed("up"):
-		move_vec.x += 1
-	if mouse_pos.x > screen_size.x - screen_edge_threshold or Input.is_action_pressed("right"):
-		move_vec.z += 1
+		move_vec -= transform.basis.z
 	if mouse_pos.y > screen_size.y - screen_edge_threshold or Input.is_action_pressed("down"):
-		move_vec.x -= 1
+		move_vec += transform.basis.z
+	if mouse_pos.x < screen_edge_threshold or Input.is_action_pressed("left"):
+		move_vec -= transform.basis.x
+	if mouse_pos.x < screen_edge_threshold or Input.is_action_pressed("right"):
+		move_vec += transform.basis.x
 	
-	move_vec = move_vec.rotated(Vector3(0,1,0),rotation_degrees.y).normalized()
-	global_translate(move_vec * delta * screen_scroll_speed)
+	move_vec = move_vec.normalized()
+	translation += move_vec * delta * screen_scroll_speed
 	
+	#Rotation
+	if Input.is_action_pressed("q"):
+		rotation_degrees.y += delta * screen_scroll_speed * 20
+	elif Input.is_action_pressed("e"):
+		rotation_degrees.y -= delta * screen_scroll_speed * 20
+
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -42,11 +36,12 @@ func _input(event):
 		
 		if event.pressed:
 			var current_pos = self.get_translation()
+			
 			match event.button_index:
 				BUTTON_WHEEL_UP:
-					set_translation(Vector3(current_pos.x,current_pos.y - .2 , current_pos.z))
+					if current_pos.y > 3: set_translation(Vector3(current_pos.x,current_pos.y - .2 , current_pos.z))
 					
 				BUTTON_WHEEL_DOWN:
-					set_translation(Vector3(current_pos.x,current_pos.y + .2 , current_pos.z))
+					if current_pos.y < 10: set_translation(Vector3(current_pos.x,current_pos.y + .2 , current_pos.z))
 				
 	
